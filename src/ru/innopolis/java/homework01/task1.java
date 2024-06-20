887d6afcdf1610d533148ed4295b60a92e5eb15d
Elizaveta <lizvt.and@gmail.com>
2024-06-20 16:01:15 +0300

    task 1 and task 2

---
+++ src/ru/innopolis/java/homework01/task1.java
@@ -0,0 +1,7 @@
+package ru.innopolis.java.homework01;
+
+public class task1 {
+    public static void main(String[] args) {
+        System.out.print(1 + "   " + 2 + "   " + 3 + "   " + 4 );
+    }
+}
