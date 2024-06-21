30734bf0401585629450e2ef2b75747ec43db6d5
Elizaveta <lizvt.and@gmail.com>
2024-06-21 12:05:12 +0300

    TV and App new

--- src/ru/innopolis/java/homework05/App.java
+++ src/ru/innopolis/java/homework05/App.java
@@ -1,11 +1,12 @@
 package ru.innopolis.java.homework05;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        TV [] tvs = new TV[10];
+        TV [] tvs = new TV[4];
         for (int i = 0; i < tvs.length; i++){
             System.out.print("Включить телевизор? (1 - да, 2 - нет) : ");
             int choice = sc.nextInt();
@@ -20,6 +21,16 @@ public class App {
                 case 2:
                     tvs[i] = new TV(false, 0, 0);
                     break;
+            }
+        }
+
+        for (int i = 0; i < tvs.length - 1; i++) {
+            for(int j = 0; j < tvs.length - i - 1; j++) {
+                if(tvs[j + 1].getChannel() < tvs[j].getChannel()) {
+                    TV swap = tvs[j];
+                    tvs[j] = tvs[j + 1];
+                    tvs[j + 1] = swap;
+                }
             }
         }
 
