3c1c67dbff3e87d9b71ac6aedfacaf535547eca3
Elizaveta <lizvt.and@gmail.com>
2024-08-07 11:45:40 +0300

    правки по комментариям после проверки в классах Product, Person и App, добавление классов PersonConverter и ProductConverter

--- src/ru/innopolis/java/homework06/App.java
+++ src/ru/innopolis/java/homework06/App.java
@@ -1,6 +1,5 @@
 package ru.innopolis.java.homework06;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
@@ -10,24 +9,20 @@ public class App {
         System.out.println("Введите имена покупателей и суммы денег у них");
         String persons = sc.nextLine();
 
-        try {Person [] personsArray = massCreate.createPersons(persons);
+        try {Person [] personsArray = PersonConverter.convertFromString(persons);
             System.out.println("Введите наименования продуктов и их стоимость");
             String products = sc.nextLine();
-            Product [] productsArray = massCreate.createProducts(products);
+            Product [] productsArray = ProductConverter.convertFromString(products);
 
             System.out.println("Введите имя покупателя и выбранный продукт либо END для завершения");
             String s = sc.nextLine();
             s = s.trim();
             while (!s.equals("END")) {
                 String name = s.substring(0, s.lastIndexOf(" "));
-                int idPerson = massCreate.getPersons(personsArray, name);
+                int idPerson = PersonConverter.getPersonIndexByName(personsArray, name);
                 String productName = s.substring(s.lastIndexOf(" ") + 1);
-                int idProduct = massCreate.getProducts(productsArray, productName);
-                if (personsArray[idPerson].getMoney() >= productsArray[idProduct].getProductPrice()) {
-                    ArrayList<Product> pack = personsArray[idPerson].getPack();
-                    pack.add(productsArray[idProduct]);
-                    personsArray[idPerson].setPack(pack);
-                    personsArray[idPerson].setMoney(personsArray[idPerson].getMoney() - productsArray[idProduct].getProductPrice());
+                int idProduct = ProductConverter.getProductIndexByProductName(productsArray, productName);
+                if (personsArray[idPerson].addProductToPack(productsArray[idProduct])) {
                     System.out.println(personsArray[idPerson].getName() + " купил(а) " + productsArray[idProduct].getProductName());
                 }
                 else {
@@ -38,14 +33,11 @@ public class App {
                 s = s.trim();
             }
 
-            for (int i = 0; i < personsArray.length; i++) {
-                System.out.println(personsArray[i].toString());
+            for (Person person : personsArray) {
+                System.out.println(person.toString());
             }
         }
-        catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-        catch (NullPointerException e) {
+        catch (IllegalArgumentException | NullPointerException e) {
             System.out.println(e.getMessage());
         }
     }
