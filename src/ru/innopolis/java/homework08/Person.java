3aa7fca32d5eb442cf8f08cb05773a25a2d26a5c
Elizaveta <lizvt.and@gmail.com>
2024-08-12 09:26:58 +0300

    классы Product, Person, App,PersonConverter и ProductConverter

---
+++ src/ru/innopolis/java/homework08/Person.java
@@ -0,0 +1,96 @@
+package ru.innopolis.java.homework08;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int money;
+    private List<Product> pack;
+
+    public Person(String name, int money) {
+        if (Objects.isNull(name)) {
+            throw new NullPointerException("Name cannot be null");
+        } else {
+            this.name = name;
+        }
+        if (money < 0) {
+            throw new IllegalArgumentException("Деньги не могут быть отрицательными");
+        } else {
+            this.money = money;
+        }
+        this.pack = new ArrayList<>();
+    }
+
+    public void setMoney(int money) {
+        if (money < 0) {
+            throw new IllegalArgumentException("Деньги не могут быть отрицательными");
+        } else {
+            this.money = money;
+        }
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setName(String name) {
+        if (Objects.isNull(name)) {
+            throw new NullPointerException("Name cannot be null");
+        } else {
+            this.name = name;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPack(List<Product> pack) {
+        this.pack = pack;
+    }
+
+    public List<Product> getPack() {
+        return this.pack;
+    }
+
+    public boolean addProductToPack(Product product) {
+        if (this.getMoney() >= product.getProductPrice()){
+            this.pack.add(product);
+            this.setMoney(this.getMoney() - product.getProductPrice());
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return money == person.money && Objects.equals(name, person.name) && Objects.deepEquals(pack, person.pack);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, money, Arrays.hashCode(pack.toArray()));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(this.name);
+        s.append(" - ");
+        if (pack.isEmpty()) {
+            s.append("Ничего не куплено");
+        }
+        else {
+            s.append(pack.toString(), 1, pack.toString().length() - 1);
+        }
+        return s.toString();
+    }
+}
---
+++ src/ru/innopolis/java/homework08/Product.java
@@ -0,0 +1,64 @@
+package ru.innopolis.java.homework08;
+
+import java.util.Objects;
+
+public class Product {
+    private String productName;
+    private int productPrice;
+
+    public Product(String productName, int productPrice) {
+        if (productName == null || productName.isEmpty()){
+            throw new NullPointerException("Имя не может быть пустым");
+        }
+        else {
+            this.productName = productName;
+        }
+        if (productPrice < 0){
+            throw new IllegalArgumentException("Цена не может быть отрицательной");
+        }
+        else {
+            this.productPrice = productPrice;
+        }
+    }
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        if (productName == null || productName.isEmpty()){
+            throw new NullPointerException("Имя не может быть пустым");
+        }
+        else {
+            this.productName = productName;
+        }
+    }
+    public int getProductPrice() {
+        return productPrice;
+    }
+    public void setProductPrice(int productPrice) {
+        if (productPrice < 0){
+            throw new IllegalArgumentException("Цена не может быть отрицательной");
+        }
+        else {
+            this.productPrice = productPrice;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productPrice == product.productPrice && Objects.equals(productName, product.productName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productName, productPrice);
+    }
+
+    @Override
+    public String toString() {
+        return productName;
+    }
+}
